e72b91a26bb480de53d6e3b799172d76758608d2
ethandpowers <ethandpowers@gmail.com>
2021-07-18 21:21:46 -0400

    Renamed to BibleResearchApp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BibleResearchApp2
+# BibleResearchApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.0.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/BibleResearchApp2'),
+      dir: require('path').join(__dirname, './coverage/BibleResearchApp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
